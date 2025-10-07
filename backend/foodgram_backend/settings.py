d5245e108dd1ba0e3d6759c2bcfe268213a5ad58
Elena Aksenova <lenta4ka2010@mail.ru>
2025-10-08 00:56:23 +0300

    test

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 from dotenv import load_dotenv
 
 from foodgram_backend.constants import PAGE_SIZE
@@ -9,7 +10,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '84.201.137.106']
