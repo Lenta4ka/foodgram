a5a571d3e36fa9694e7db281928b10b374e8cd2e
Elena Aksenova <lenta4ka2010@mail.ru>
2025-10-08 00:06:33 +0300

    tags_and_ing

--- backend/recipes/management/commands/load_tags.py
+++ backend/recipes/management/commands/load_tags.py
@@ -8,8 +8,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         data = [
-            {'name': 'Завтрак', 'color': '#E26C2D', 'slug': 'breakfast'},
-            {'name': 'Обед', 'color': '#49B64E', 'slug': 'dinner'},
-            {'name': 'Ужин', 'color': '#8775D2', 'slug': 'supper'}]
+            {'name': 'Завтрак', 'slug': 'breakfast'},
+            {'name': 'Обед', 'slug': 'dinner'},
+            {'name': 'Ужин', 'slug': 'supper'}]
         Tag.objects.bulk_create(Tag(**tag) for tag in data)
         self.stdout.write(self.style.SUCCESS('Все тэги загружены!'))
